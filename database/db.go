ee6dcdf24c5209d8bbd50b80216bc58a4384a035
Bruno Henrique <brunodorea@outlook.com.br>
2023-01-27 12:36:57 -0300

    ➕ build: criação do arquivo ec2.yml

--- database/db.go
+++ database/db.go
@@ -18,9 +18,9 @@ func ConectaComBancoDeDados() {
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
 	dbname := os.Getenv("DBNAME")
-	port := os.Getenv("PORT")
+	dbport := os.Getenv("DBPORT")
 	
-	stringDeConexao := "host="+ host +" user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
+	stringDeConexao := "host="+ host +" user=" + user + " password=" + password + " dbname=" + dbname + " port=" + dbport + " sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados")
